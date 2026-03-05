ac8035b255a241d3a5842abd7101ea6669127f1b
João Vitor <joao.vitor.dev.br@gmail.com>
2026-03-05 11:44:21 -0300

    Adicionar exemplos de estruturas de controle em Python.
    Inclui exemplos de if/else, for e while.

---
+++ controle.py
@@ -0,0 +1,23 @@
+#Estruturas de controle
+
+# IF / ELSE
+
+idade = 20 
+if idade >= 18:
+    print("Você é maior de idade")
+else:
+    print("Você é menor de idade")
+
+
+# FOR 
+
+for i in range (1, 10):
+    print("Número:", i)
+
+
+# WHILE
+
+cont = 1
+while cont <= 5:
+    print("Contagem:", cont)
+cont += 1
